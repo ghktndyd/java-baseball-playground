ff8c2d704bf64891f1cbadf5780d3c2122603930
sooyong <hwasuyong99@naver.com>
2023-11-19 08:30:15 +0900

    요구사항 1-1을 해결했습니다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,4 +11,15 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    @DisplayName("요구사항 1-1")
+    void stringSplit() {
+        String splitTarget = "1,2";
+
+        String[] splitResult = splitTarget.split(",");
+
+        assertThat(splitTarget).contains("1", "2");
+    }
+
 }
