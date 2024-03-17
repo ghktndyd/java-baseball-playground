eb0396288d33d886ce1f59720b89953a6ae01324
ghktndyd <hwasuyong99@naver.com>
2024-03-17 21:48:02 +0900

    feat: 계산기 기능만 구현

--- src/main/java/stringcalculator/my/StringCalculatorApp.java
+++ src/main/java/stringcalculator/my/StringCalculatorApp.java
@@ -1,9 +1,36 @@
 package stringcalculator.my;
 
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class StringCalculatorApp {
     public static void main(String[] args) {
-        StringCalculator stringCalculator = new StringCalculator();
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        int calculate = calculate(value);
+        System.out.println("calculate = " + calculate);
+    }
+
+    private static int calculate(String value) {
+        String[] values = value.split(" ");
+        int result = Integer.parseInt(values[0]);
+
+        for (int i = 1; i < values.length; i += 2) {
+            String operator = values[i];
+            int number = Integer.parseInt(values[i + 1]);
+
+            if (operator.equals("+")) {
+                result += number;
+            } else if (operator.equals("-")) {
+                result -= number;
+            } else if (operator.equals("*")) {
+                result *= number;
+            } else if (operator.equals("/")) {
+                result /= number;
+            } else {
+                throw new IllegalArgumentException("올바르지 않은 연산자입니다 : " + operator);
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,9 +1,5 @@
 package study;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 public class CalculatorTest {
 
-    private String[] operator;
 }
