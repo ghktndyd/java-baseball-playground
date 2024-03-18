95d984ad3ce5dd45cd160f56507e6eb2ba845ed5
ghktndyd <hwasuyong99@naver.com>
2024-03-18 14:53:32 +0900

    feat: splitTest

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,5 +1,18 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CalculatorTest {
 
+    @Test
+    @DisplayName("split() 테스트")
+    void splitTest() {
+        String value = "2 + 3 * 4 / 2";
+        String[] values = value.split(" ");
+
+        assertThat(values).containsExactly("2", "+", "3", "*", "4", "/", "2");
+    }
 }
